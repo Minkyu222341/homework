5941ed383927e7d04253df42d3cbf80988f5d161
Minkyu222341 <qkralsrb2040@naver.com>
2023-02-03 11:11:07 +0900

    [style] 변수명정리

--- src/main/java/callbus/homework/util/MemberTypeCheck.java
+++ src/main/java/callbus/homework/util/MemberTypeCheck.java
@@ -8,18 +8,18 @@ public class MemberTypeCheck {
 
     private static final String ANONYMOUS = "ANONYMOUS";
 
-    public Long memberIdCheck(String userInfo) {
-        if (userInfo != null) {
-            String memberId = userInfo.split(" ")[1];
+    public Long memberIdCheck(String memberInfo) {
+        if (memberInfo != null) {
+            String memberId = memberInfo.split(" ")[1];
             return Long.valueOf(memberId);
         } else {
             return -1L;
         }
     }
 
-    public String accountTypeCheck(String userInfo) {
-        if (userInfo != null) {
-            return userInfo.split(" ")[0];
+    public String accountTypeCheck(String memberInfo) {
+        if (memberInfo != null) {
+            return memberInfo.split(" ")[0];
         } else {
             return ANONYMOUS;
         }
